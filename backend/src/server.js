3d4ff0e2c55a10049df85b17b1b18f40b4dd5d97
hritxx <hriteekroy1869@gmail.com>
2026-01-03 05:25:46 +0530

    fix: correct static file paths in server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,17 +6,19 @@ const app = express();
 const PORT = ENV.PORT;
 const NODE_ENV = ENV.NODE_ENV;
 
+const __dirname = path.resolve();
+
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
 });
 
 if (NODE_ENV === "production") {
-  app.use(express.static(path.join(process.cwd(), "admin/dist")));
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
 }
 
-app.get("*", (req, res) => {
+app.get("/{*any}", (req, res) => {
   if (NODE_ENV === "production") {
-    res.sendFile(path.join(process.cwd(), "admin/dist/index.html"));
+    res.sendFile(path.join(__dirname, "admin", "dist", "index.html"));
   } else {
     res.status(404).json({ message: "Not Found" });
   }
